e8318d3427c35eec5219b3ebce59a2edd8e11774
Luke Fritz <lukefritz@gmail.com>
2020-12-03 14:25:30 -0600

    Add support for macOS sed command (fix)

--- .init-template.sh
+++ .init-template.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-
 # This script initializes a repository deployed from this template.
+set -e;
+
+SED_COMMAND="sed -i"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS doesn't use GNU sed and has a slightly different syntax
+  SED_COMMAND="sed -i '' -E"
+fi
 
 function update_package_json() {
   local -r package_name="$1"
   echo "Enter a short description for the repository (package.json):"
   echo '  (e.g. "Contains modules for deploying and managing <type> resources.")'
   read -r description
-  sed -i "s/template-terraform-package/$package_name/" package.json
-  sed -i "s/{{package_description}}/$description/" package.json
+  eval "$SED_COMMAND 's/template-terraform-package/$package_name/g' package.json"
+  eval "$SED_COMMAND 's/{{package_description}}/$description/g' package.json"
   npm install
 }
 
@@ -17,15 +23,15 @@ function update_readme() {
   echo "Enter a long description for the repository (README.md):"
   echo '  (e.g. "This repository contains modules for deploying and managing <type> resources. <context>.")'
   read -r description
-  sed -i "s/template-terraform-package/$package_name/" README.md
-  sed -i "s/{{package_description}}/$description/" README.md
+  eval "$SED_COMMAND 's/template-terraform-package/$package_name/g' README.md"
+  eval "$SED_COMMAND 's/{{package_description}}/$description/g' README.md"
 }
 
 function main() {
   local -r repository_name=$(git remote -v | grep push | sed -e 's|.*/||' | sed -e 's/.git.*//')
 
   echo "Initializing repository from template..."
-  echo
+  echo "Using repository name as the package name ($repository_name)..."
 
   update_package_json "$repository_name"
   update_readme "$repository_name"
