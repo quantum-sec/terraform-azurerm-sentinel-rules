c318728fb72819750d4775034ec7ded291ed7d4c
Gerard Setho <gerard.setho@quantum.security>
2022-04-18 10:28:15 +0800

    XDR-1212: Update description of outputs (feat)

--- modules/sentinel-automation-rule/outputs.tf
+++ modules/sentinel-automation-rule/outputs.tf
@@ -1,3 +1,4 @@
 output "id" {
-  value = azurerm_sentinel_automation_rule.automation_rule.id
+  description = "ID of the deployed automation rule."
+  value       = azurerm_sentinel_automation_rule.automation_rule.id
 }
