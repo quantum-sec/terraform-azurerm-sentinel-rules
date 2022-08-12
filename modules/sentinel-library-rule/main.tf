3cfa885f093a3305b830838262ecd9a9ed7a6734
deepanshumarwah <deepanshumarwah@gmail.com>
2022-08-12 09:35:29 +0800

    XDR-3582: Update default value logic (patch)

--- modules/sentinel-library-rule/main.tf
+++ modules/sentinel-library-rule/main.tf
@@ -9,8 +9,8 @@ locals {
   create_incident          = try(lookup(local.rule_data["incidentConfiguration"], "createIncident", false), false)
   create_incident_grouping = try(lookup(local.rule_data["incidentConfiguration"], "grouping", {}), {})
   entity_mappings          = try(lookup(local.rule_data, "entityMappings", []), [])
-
-  rule_name = element(local.path_elements, length(local.path_elements) - 1)
+  trigger_operator         = try(lookup(local.rule_data["triggerOperator"]), "gt")
+  rule_name                = element(local.path_elements, length(local.path_elements) - 1)
 }
 
 module "rule" {
@@ -21,7 +21,7 @@ module "rule" {
   name         = local.rule_name
   display_name = try(lookup(local.rule_data, "displayName", "name"), local.rule_name)
   description  = lookup(local.rule_data, "description", local.rule_name)
-  severity     = title(local.rule_data["severity"])
+  severity     = lookup(title(local.rule_data["severity"]), "Low")
   enabled      = lookup(local.rule_data, "enabled", true)
   tactics      = lookup(local.rule_data, "tactics", [])
 
@@ -29,8 +29,8 @@ module "rule" {
   query_frequency = length(regexall(".*[Dd]$", local.rule_data["queryFrequency"])) > 0 ? "P${upper(local.rule_data["queryFrequency"])}T0H" : "PT${upper(local.rule_data["queryFrequency"])}"
   query_period    = length(regexall(".*[Dd]$", local.rule_data["queryPeriod"])) > 0 ? "P${upper(local.rule_data["queryPeriod"])}T0H" : "PT${upper(local.rule_data["queryPeriod"])}"
 
-  trigger_operator  = local.rule_data["triggerOperator"] == "gt" ? "GreaterThan" : local.rule_data["triggerOperator"] == "lt" ? "LessThan" : local.rule_data["triggerOperator"]
-  trigger_threshold = local.rule_data["triggerThreshold"]
+  trigger_operator  = local.trigger_operator == "gt" ? "GreaterThan" : local.trigger_operator == "lt" ? "LessThan" : local.trigger_operator
+  trigger_threshold = lookup(local.rule_data["triggerThreshold"], "0")
 
   suppression_duration = try(local.rule_data["suppressionEnabled"] == true ? length(regexall(".*[YyMmDd]$", local.rule_data["suppressionDuration"])) > 0 ? "P${upper(local.rule_data["suppressionDuration"])}T0H" : "PT${upper(local.rule_data["suppressionDuration"])}" : null, null)
   suppression_enabled  = lookup(local.rule_data, "suppressionEnabled", false)
