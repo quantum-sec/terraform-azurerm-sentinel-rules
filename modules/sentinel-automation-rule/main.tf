de275fe365de67aefed41936fc74011fc05846ff
nicholascwd <56076256+nicholascwd@users.noreply.github.com>
2023-06-06 05:39:04 +0000

    XDR-4406: Remove automation rule from soar-webhook (patch)

--- modules/sentinel-automation-rule/main.tf
+++ modules/sentinel-automation-rule/main.tf
@@ -9,34 +9,6 @@ terraform {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# FOR SOAR WEBHOOK DEPLOYMENT
-# ---------------------------------------------------------------------------------------------------------------------
-
-resource "azurerm_sentinel_automation_rule" "automation_rule" {
-  display_name               = var.display_name
-  log_analytics_workspace_id = var.log_analytics_workspace_id
-  name                       = uuidv5("dns", var.unique_prefix)
-  order                      = var.rule_order
-  enabled                    = var.enabled
-  expiration                 = var.expiration
-
-  dynamic "condition" {
-    for_each = var.conditions
-
-    content {
-      operator = condition.value["operator"]
-      property = condition.value["property"]
-      values   = condition.value["values"]
-    }
-  }
-
-  action_playbook {
-    logic_app_id = var.logic_app_id
-    order        = var.action_order
-  }
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
 # CUSTOM CONTENT DEPLOYMENT FOR AUTOMATION RULE
 # ---------------------------------------------------------------------------------------------------------------------
 
--- modules/sentinel-automation-rule/vars.tf
+++ modules/sentinel-automation-rule/vars.tf
@@ -1,52 +1,5 @@
-variable "display_name" {
-  type = string
-}
-
-variable "unique_prefix" {
-  description = "Unique string used to generate a UUID."
-  type        = string
-}
-
-# The default condition is when Status = Active
-variable "conditions" {
-  default = []
-
-  type = list(object({
-    operator = string
-    property = string
-    values   = list(string)
-  }))
-}
-
 variable "log_analytics_workspace_id" {
   type = string
-}
-
-variable "logic_app_id" {
-  type = string
-}
-
-variable "rule_order" {
-  description = "The order of this Sentinel Automation Rule"
-  type        = number
-  default     = 1
-}
-
-variable "action_order" {
-  description = "The execution order of this action"
-  type        = number
-  default     = 1
-}
-
-variable "enabled" {
-  type    = bool
-  default = false
-}
-
-variable "expiration" {
-  description = "The UTC time in RFC3339 format"
-  type        = string
-  default     = null
 }
 
 variable "default_automation_rules" {
